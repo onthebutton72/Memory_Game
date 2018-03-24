efdfa2ad17a1ab46b1f9e1493dd3633307cbe427
jm2424 <35273190+onthebutton72@users.noreply.github.com>
2018-03-24 15:28:14 -0700

    feat: card match module
    Complete the basic card match portion of the script.

--- js/app.js
+++ js/app.js
@@ -2,8 +2,6 @@
 var cards = [1,1,2,2,3,3,4,4,5,5,6,6];
 var matchArray = [];
 
-
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(cards) {
     var currentIndex = cards.length, temporaryValue, randomIndex;
@@ -27,25 +25,44 @@ function mapToCards(){
 }
 
 // Function to get the HTML contents of the first element in the set of matched
-// elements (data-card) and display the number assigned to the card inside a
-// new paragraph.
+// elements (data-card) and display the number assigned to the card.
 function onClick(){
   $('.card').click(function(){
-    $(this).html('<p>' + $(this).data('card') + '</p>');
-    var cardSelected = ($(this).data('card'));
-    // Check to see if array is less than 2 elements in length.  If true then
-    // append the array with the card value.
-    if(matchArray.length < 2){
-      matchArray.push (cardSelected);
-      }
-    console.log (matchArray);
-    // Check to see if the first card value matches the second card value.
-    if(matchArray[0] == matchArray[1]){
-      console.log("match");
-    }else{
-      console.log("no match");
-    }
+    $(this).html('<p>' + $(this).data('card') + '</p>').addClass('chosen');
+    matchCards();
   });
+}
+
+// Function to check if all cards match => win game.
+function allMatch(){
+  if($('.unchosen').length == 0){
+    $('.container').html('<h1>You Won the Game!</h1>');
+  }
+}
+
+// Function to match two cards and if match, make them disappear.
+function matchCards(){
+  var choices = 2;
+  var hideCard = 0;
+  if($('.chosen').length == choices){
+    if ($('.chosen').first().data('card') == $('.chosen').last().data('card')){
+      // Make chosen cards disappear.
+      $('.chosen').each(function(){
+        $(this).animate({opacity: hideCard}).removeClass('unchosen');
+      });
+      $('.chosen').each(function(){
+        $(this).removeClass('chosen');
+      });
+      allMatch();
+    }else{
+      // Flip cards that do not match.
+      setTimeout(function(){
+        $('.chosen').each(function(){
+          $(this).html('').removeClass('chosen');
+        })
+      }, 1000);
+    }
+  }
 }
 
 // Run the methods
